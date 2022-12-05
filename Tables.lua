b6a3d6cbf42610bd7ffe3e407585811fc3dedcfe
Audrey Riley <darkdoom1337@gmail.com>
2022-12-05 13:03:38 -0700

    initial commit

---
+++ Tables.lua
@@ -0,0 +1,474 @@
+--[[
+    Written By: Uwu/Darkdoom 12/5/2022
+    Description: This class adds additional (linq-like) functionality to lua tables
+    Notes: This is a work in progress
+]]
+
+local table = require('table')
+
+local Tables = {
+
+}
+
+local MetaTable = {
+    __call = T,
+    __index = Tables,
+}
+
+function T(...)
+    local arg = {...}
+    local t = {}
+    if(type(arg[1]) == 'table')then
+        t = arg[1]
+    else
+        for i = 1, #arg do
+            t[i] = arg[i]
+        end
+    end
+    return setmetatable(t, MetaTable)
+end
+
+function Tables.Length(t)
+    local count = 0
+    for _ in t:It() do
+        count = count + 1
+    end
+    return count
+end
+
+function Tables.DeepCopy(t)
+    local copy = {}
+    for k, v in pairs(t) do
+        if type(v) == 'table' then
+            v = Tables.DeepCopy(v)
+        end
+        copy[k] = v
+    end
+    return T(copy)
+end
+
+function Tables.ShallowCopy(t)
+    local copy = {}
+    for k, v in pairs(t) do
+        copy[k] = v
+    end
+    return T(copy)
+end
+
+function Tables.Clear(t)
+    for k, v in pairs(t) do
+        t[k] = nil
+    end
+end
+
+function Tables.InsertAt(t, value, index)
+    if(index == nil)then
+        table.insert(t, value)
+    else
+        table.insert(t, index, value)
+    end
+end
+
+function Tables.Insert(t, value)
+    table.insert(t, value)
+end
+
+function Tables.RemoveAt(t, index)
+    table.remove(t, index)
+end
+
+function Tables.Remove(t, value)
+    for i, v in pairs(t) do
+        if(v == value)then
+            table.remove(t, i)
+            return
+        end
+    end
+end
+
+function Tables.Contains(t, value)
+    for v in t:It() do
+        if(v == value)then
+            return true
+        end
+    end
+    return false
+end
+
+function Tables.Any(t, fn)
+    for v in t:It() do
+        if(fn(v))then
+            return true
+        end
+    end
+    return false
+end
+
+function Tables.All(t, fn)
+    for v in t:It() do
+        if(not fn(v))then
+            return false
+        end
+    end
+    return true
+end
+
+function Tables.Append(t, val)
+    if(type(val) == 'table')then
+        for v in val:It() do
+            table.insert(t, v)
+        end
+    else
+        table.insert(t, val)
+    end
+end
+
+function Tables.Average(t)
+    local sum = 0
+    for v in t:It() do
+        sum = sum + v
+    end
+    return sum / Tables.Length(t)
+end
+
+function Tables.Chunk(t, maxChunkSize)
+    local chunks = T()
+    local chunk = T()
+    for i, v in pairs(t) do
+        if(i % maxChunkSize == 0)then
+            chunks:Insert(chunk)
+            chunk = T()
+        else
+            chunk:Insert(v)
+        end
+    end
+    if(chunk:Length() > 0)then
+        chunks:Insert(chunk)
+    end
+    return chunks
+end
+
+function Tables.Concat(t1, t2)
+    local new = T()
+    for v in t1:It() do
+        new:Insert(v)
+    end
+    for v in t2:It() do
+        new:Insert(v)
+    end
+    return new
+end
+
+function Tables.Count(t, fn)
+    local count = 0
+    for v in t:It() do
+        if(fn(v))then
+            count = count + 1
+        end
+    end
+    return count
+end
+
+function Tables.Distinct(t)
+    local new = T()
+    for v in t:It() do
+        if(not new:Contains(v))then
+            new:Insert(v)
+        end
+    end
+    return new
+end
+
+function Tables.Distinct(t, fnComparison)
+    local new = T()
+    for v in t:It() do
+        if(not new:Any(function(x) return fnComparison(x, v) end))then
+            new:Insert(v)
+        end
+    end
+    return new
+end
+
+function Tables.ElementAt(t, index)
+    return t[index]
+end
+
+function Tables.Empty(t)
+    return Tables.Length(t) == 0
+end
+
+function Tables.First(t)
+    return t[1]
+end
+
+function Tables.FirstBy(t, fn)
+    for v in t:It() do
+        if(fn(v))then
+            return v
+        end
+    end
+end
+
+function Tables.FirstOrDefault(t)
+    if(Tables.Length(t) > 0)then
+        return Tables.First(t)
+    end
+    return nil
+end
+
+function Tables.FirstOrDefaultBy(t, fn)
+    if(Tables.Length(t) > 0)then
+        return Tables.First(t, fn)
+    end
+    return nil
+end
+
+function Tables.ForEach(t, fn)
+    for v in t:It() do
+        fn(v)
+    end
+end
+
+function Tables.It(t)
+    local i = 0
+    return function()
+        i = i + 1
+        return t[i]
+    end
+end
+
+function Tables.GroupBy(t, fn)
+    local groups = T()
+    for v in t:It() do
+        local key = fn(v)
+        if(not groups:ContainsKey(key))then
+            groups[key] = T()
+        end
+        groups[key]:Insert(v)
+    end
+    return groups
+end
+
+function Tables.Join(t1, t2, fnJoin)
+    local new = T()
+    for v in t1:It() do
+        for v2 in t2:It() do
+            if(fnJoin(v, v2))then
+                new:Insert(v)
+            end
+        end
+    end
+    return new
+end
+
+function Tables.Last(t)
+    return t[Tables.Length(t)]
+end
+
+function Tables.Last(t, fn)
+    for i = Tables.Length(t), 1, -1 do
+        if(fn(t[i]))then
+            return t[i]
+        end
+    end
+end
+
+function Tables.LastOrDefault(t)
+    if(Tables.Length(t) > 0)then
+        return Tables.Last(t)
+    end
+    return nil
+end
+
+function Tables.LastOrDefault(t, fn)
+    if(Tables.Length(t) > 0)then
+        return Tables.Last(t, fn)
+    end
+    return nil
+end
+
+function Tables.Max(t)
+    local max = t[1]
+    for v in t:It() do
+        if(v > max)then
+            max = v
+        end
+    end
+    return max
+end
+
+function Tables.Max(t, fn)
+    local max = t[1]
+    for v in t:It() do
+        if(fn(v) > fn(max))then
+            max = v
+        end
+    end
+    return max
+end
+
+function Tables.Min(t)
+    local min = t[1]
+    for v in t:It() do
+        if(v < min)then
+            min = v
+        end
+    end
+    return min
+end
+
+function Tables.Min(t, fn)
+    local min = t[1]
+    for v in t:It() do
+        if(fn(v) < fn(min))then
+            min = v
+        end
+    end
+    return min
+end
+
+function Tables.OrderBy(t, fn)
+    local new = T()
+    for v in t:It() do
+        new:Insert(v)
+    end
+    table.sort(new, fn)
+    return new
+end
+
+function Tables.OrderByDescending(t, fn)
+    local new = T()
+    for v in t:It() do
+        new:Insert(v)
+    end
+    table.sort(new, function(a, b) return fn(b, a) end)
+    return new
+end
+
+function Tables.Prepend(t, v)
+    local new = T()
+    new:Insert(v)
+    for v in t:It() do
+        new:Insert(v)
+    end
+    return new
+end
+
+function Tables.PrependRange(t1, t2)
+    local new = T()
+    for v in t2:It() do
+        new:Insert(v)
+    end
+    for v in t1:It() do
+        new:Insert(v)
+    end
+    return new
+end
+
+function Tables.Reverse(t)
+    local new = T()
+    for i = Tables.Length(t), 1, -1 do
+        new:Insert(t[i])
+    end
+    return new
+end
+
+function Tables.Select(t, fn)
+    local new = T()
+    for v in t:It() do
+        new:Insert(fn(v))
+    end
+    return new
+end
+
+function Tables.SequenceEqual(t1, t2)
+    if(Tables.Length(t1) ~= Tables.Length(t2))then
+        return false
+    end
+    for i = 1, Tables.Length(t1) do
+        if(t1[i] ~= t2[i])then
+            return false
+        end
+    end
+    return true
+end
+
+function Tables.Skip(t, n)
+    local new = T()
+    for i = n + 1, Tables.Length(t) do
+        new:Insert(t[i])
+    end
+    return new
+end
+
+function Tables.SkipWhile(t, fn)
+    local new = T()
+    local i = 1
+    while(fn(t[i]))do
+        i = i + 1
+    end
+    for i = i, Tables.Length(t) do
+        new:Insert(t[i])
+    end
+    return new
+end
+
+function Tables.Sum(t)
+    local sum = 0
+    for v in t:It() do
+        sum = sum + v
+    end
+    return sum
+end
+
+function Tables.Sum(t, fn)
+    local sum = 0
+    for v in t:It() do
+        sum = sum + fn(v)
+    end
+    return sum
+end
+
+function Tables.Take(t, n)
+    local new = T()
+    for i = 1, n do
+        new:Insert(t[i])
+    end
+    return new
+end
+
+function Tables.TakeWhile(t, fn)
+    local new = T()
+    for v in t:It() do
+        if(fn(v))then
+            new:Insert(v)
+        else
+            break
+        end
+    end
+    return new
+end
+
+function Tables.ToLookup(t, fn)
+    local new = T()
+    for v in t:It() do
+        local key = fn(v)
+        if(not new[key])then
+            new[key] = T()
+        end
+        new[key]:Insert(v)
+    end
+    return new
+end
+
+function Tables.Where(t, fn)
+    local new = T()
+    for v in t:It() do
+        if(fn(v))then
+            new:Insert(v)
+        end
+    end
+    return new
+end
+
+return Tables
---
+++ Vector3.lua
@@ -0,0 +1,141 @@
+--[[
+    Written By: Uwu/Darkdoom 12/5/2022
+    Description: This is a simple vector3 class that can be used to represent a 3d point in space and operate on it.
+    Notes: follows memory layout for position order(x,z,y).
+]]
+
+local Vector3 = {
+    x = 0,
+    z = 0,
+    y = 0
+}
+
+local MetaTable = {
+    __call = Vec3,
+    __index = Vector3,
+    __class = "Vector3",
+    __tostring = function(self)
+        return string.format("Vector3: %.2f, %.2f, %.2f", self.x, self.z, self.y)
+    end,
+    __metatable = "Vector3 is a protected metatable",
+    __add = function(self, other)
+        return Vec3(self.x + other.x, self.z + other.z, self.y + other.y)
+    end,
+    __sub = function(self, other)
+        return Vec3(self.x - other.x, self.z - other.z, self.y - other.y)
+    end,
+    __mul = function(self, other)
+        return Vec3(self.x * other.x, self.z * other.z, self.y * other.y)
+    end,
+    __div = function(self, other)
+        return Vec3(self.x / other.x, self.z / other.z, self.y / other.y)
+    end,
+    __eq = function(self, other)
+        return self.x == other.x and self.z == other.z and self.y == other.y
+    end,
+    __lt = function(self, other)
+        return self.x < other.x and self.z < other.z and self.y < other.y
+    end,
+    __le = function(self, other)
+        return self.x <= other.x and self.z <= other.z and self.y <= other.y
+    end,
+    __unm = function(self)
+        return Vec3(-self.x, -self.z, -self.y)
+    end,
+    __len = function(self)
+        return math.sqrt(self.x * self.x + self.z * self.z + self.y * self.y)
+    end,
+    __concat = function(self, other)
+        return string.format("v1:%s v2:%s", tostring(self), tostring(other))
+    end
+}
+
+function Vec3(...)
+    local arg = {...}
+    local self = setmetatable({}, MetaTable)
+    self.x = 0
+    self.y = 0
+    self.z = 0
+    if(type(arg[1]) == "number")then
+        self.x = arg[1]
+        self.z = arg[2]
+        self.y = arg[3]
+    elseif(type(arg[1]) == "table")then
+        self.x = arg[1][1]
+        self.z = arg[1][2]
+        self.y = arg[1][3]
+    end
+    return self
+end
+
+function Vector3:New(x, z, y)
+    local self = setmetatable({}, MetaTable)
+    self.x = x
+    self.z = z
+    self.y = y
+    return self
+end
+
+function Vector3:Distance(other)
+    return math.sqrt((self.x - other.x) * (self.x - other.x) + (self.z - other.z) * (self.z - other.z) + (self.y - other.y) * (self.y - other.y))
+end
+
+function Vector3:Dot(other)
+    return self.x * other.x + self.z * other.z + self.y * other.y
+end
+
+function Vector3:Cross(other)
+    return Vec3(self.y * other.z - self.z * other.y, self.z * other.x - self.x * other.z, self.x * other.y - self.y * other.x)
+end
+
+function Vector3:Normalize()
+    local len = math.sqrt(self.x * self.x + self.z * self.z + self.y * self.y)
+    self.x = self.x / len
+    self.z = self.z / len
+    self.y = self.y / len
+end
+
+function Vector3:Normalized()
+    local len = math.sqrt(self.x * self.x + self.z * self.z + self.y * self.y)
+    return Vec3(self.x / len, self.z / len, self.y / len)
+end
+
+function Vector3:Angle(other)
+    return math.acos(self:Dot(other) / (self:Length() * other:Length()))
+end
+
+function Vector3:Project(other)
+    return other:Normalized() * self:Dot(other:Normalized())
+end
+
+function Vector3:ProjectOnPlane(other)
+    return self - self:Project(other)
+end
+
+function Vector3:Reflect(other)
+    return self - other * 2 * self:Dot(other)
+end
+
+function Vector3:RotateAround(axis, angle)
+    local sin = math.sin(angle)
+    local cos = math.cos(angle)
+    local t = 1 - cos
+    local x = self.x
+    local y = self.y
+    local z = self.z
+    local x2 = axis.x * axis.x
+    local y2 = axis.y * axis.y
+    local z2 = axis.z * axis.z
+    local xy = axis.x * axis.y
+    local xz = axis.x * axis.z
+    local yz = axis.y * axis.z
+    local xs = axis.x * sin
+    local ys = axis.y * sin
+    local zs = axis.z * sin
+    self.x = (t * x2 + cos) * x + (t * xy - zs) * y + (t * xz + ys) * z
+    self.y = (t * xy + zs) * x + (t * y2 + cos) * y + (t * yz - xs) * z
+    self.z = (t * xz - ys) * x + (t * yz + xs) * y + (t * z2 + cos) * z
+end
+
+return Vector3
+
